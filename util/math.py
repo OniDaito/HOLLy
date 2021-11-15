ddf1017b585ec1cccdd99175d1eb9738e30b91ec
Benjamin Blundell <me@benjamin.computer>
2021-11-15 18:14:55 +0000

    Minor fix in loader

--- util/math.py
+++ util/math.py
@@ -179,6 +179,9 @@ class Points:
         self.size = len(self.data)
         return self
 
+    def to_array(self) -> array:
+        return array(self.data)
+
     def __next__(self) -> Point:
         if self.counter >= self.size:
             self.counter = 0
